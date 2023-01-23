1a714dd40c645220b1a5a4669ed35018574a9a5b
Desire Barine <desirebarine16@gmail.com>
2023-01-23 23:22:49 +0100

    tasks

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines Helper Function"""
+
+def index_range(page, page_size):
+    """return a tuple of size two containing a start index and an end index
+    """
+
+    return ((page - 1) * page_size, page * page_size)
